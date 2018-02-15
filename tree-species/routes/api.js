62dd437395c22c33698ea7c8ba8846ed6f199d5c
Eric Masseran <rico.masseran@gmail.com>
2018-02-15 15:00:22 +0100

    Return error 404 if GET page too big

--- tree-species/routes/api.js
+++ tree-species/routes/api.js
@@ -24,17 +24,30 @@ router.get('/species', function(req, res, next) {
 
 /* GET 3 tree species */
 router.get('/species/page/:nb(\\d+)', function(req, res, next) {
+  // Always Integer >= 0, due to reg exp in route
   let nb = parseInt(req.params.nb, 10);
-  let offset = nb > 0 ? ("OFFSET "+3*nb): "";
-  let query = `SELECT species FROM tree_species LIMIT 3 ${offset};`
-
-  console.log(query);
-  db.connection.query(query, function (err, rows, fields) {
+  // Check DB size
+  db.connection.query('SELECT COUNT(*) AS count FROM tree_species;', function (err, rows, fields) {
     if (err) throw err
 
-    let species = rows.map((row)=> row.species);
+    let count = rows[0].count;
 
-    res.json(species);
+    let max_page = Math.floor(count/3);
+
+    if (nb > max_page) {
+      res.status(404).send(`The last page is ${max_page}, don't try to get some after...`)
+    } else {
+      // Query requested page
+      let offset = nb > 0 ? ("OFFSET "+3*nb): "";
+      let query = `SELECT species FROM tree_species LIMIT 3 ${offset};`
+      db.connection.query(query, function (err, rows, fields) {
+        if (err) throw err
+
+        let species = rows.map((row)=> row.species);
+
+        res.json(species);
+      })
+    }
   })
 });
 
